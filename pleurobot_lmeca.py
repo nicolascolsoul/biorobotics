7467eeed795ac30a517c9ef2804ba2838da07b4f
nicolascolsoul <colsoul.nicolas@gmail.com>
2020-03-24 00:05:32 +0100

    Add the evaluation of a curve with start and end position, start angle and landing angle and height ! Just why the end effector isn't really the last joint output ?

--- pleurobot_lmeca.py
+++ pleurobot_lmeca.py
@@ -8,6 +8,9 @@ from pyrobolearn.simulators import Bullet
 from pyrobolearn.worlds import BasicWorld
 from pyrobolearn.robots import Pleurobot
 import numpy as np
+import matplotlib.pyplot as plt
+import math
+from scipy.optimize import fsolve
 
 # Create simulator
 sim = Bullet()
@@ -18,63 +21,89 @@ world.gravity =  np.array([0.0, 0.0, 0.0])
 
 # create robot
 robot = Pleurobot(sim)
-robot.position = np.array([0, 0, 0.20])
+robot.position = np.array([0, 0, 0.21])
 # print information about the robot
 robot.print_info()
 
+dt = 1./240
 link_id = robot.get_end_effector_ids(end_effector=5)
 joint_ids = robot.joints
 
-damping = 0.01
 wrt_link_id = -1
 q_idx = robot.get_q_indices(joint_ids)
-kp = 50
-kd = 0
+xd = np.array([-1.0, -0.25, 0.0])
+sphere = world.load_visual_sphere(position=xd, radius=0.05, color=(1, 0, 0, 0.5), return_body=True)
 
-sphere = world.load_visual_sphere(position=np.array([0.0, 0.0, 0.0]), radius=0.05, color=(1, 0, 0, 0.5), return_body=True)
+# Nicolas new part :
+stride_length = 0.20
+stride_height = 0.10
+start_angle = 50.0 * (np.pi/180)
+landing_angle = - 45.0 * (np.pi/180)
+# curve type : ax⁴ + bx³ + cx² + dx + e = y
+# passing by (0;0) so,
+e = 0
+# passing by (stride_length, 0) so,
+# EQ1 : a*stride_length^4 + b*stride_length^3 + c*stride_length^2 + d*stride_length + e = 0
+#passing by (stride_length/2, stride_height)
+# EQ2 : a*(stride_length/2)^4 + b*(stride_length/2)^3 + c*(stride_length/2)^2 + d*(stride_length/2) + e - stride_height = 0
+# slope at (0;0) = start_angle :
+# EQ3 : e = start_angle
+d = np.tan(start_angle)
+# slope at (stride_length;0) = landing_angle
+# EQ4 : 4*a*stride_length^3 + 3*b*stride_length^3 + 2*c*stride_length + d - anding_angle = 0
 
-a = 0.2
-w = 0.01
-# Position control using sliders
-# robot.add_joint_slider()
+def equations(p):
+    a, b, c = p
+    return (a*stride_length**4 + b*stride_length**3 + c*stride_length**2 + d*stride_length,
+    a*(stride_length/2)**4 + b*(stride_length/2)**3 + c*(stride_length/2)**2 + d*(stride_length/2) - stride_height,
+    4*a*stride_length**3 + 3*b*stride_length**2 + 2*c*stride_length + d - np.tan(landing_angle))
 
+a, b, c =  fsolve(equations, (1, 1, 1))
+
+x_plot = np.linspace(0.0, stride_length, num=1000)
+y_plot = a * np.power(x_plot, 4) + b * np.power(x_plot, 3) + c * np.square(x_plot) + d * x_plot + e
+
+plt.plot(0.0, 0.0, 'rs')
+plt.plot(0.0 + stride_length, 0.0, 'rs')
+plt.plot(stride_length/2, stride_height, 'rs')
+plt.plot(x_plot, y_plot)
+plt.show()
+
+x_t = 0.0;
+y_t = 0.0;
+v = 0.1;
+k_v = 1; # Speed sign
 # run simulator
-
-##
-
-#pos = 0;
 for t in count():
-    # robot.update_joint_slider()
-    # robot.compute_and_draw_com_position()
-    # robot.compute_and_draw_projected_com_position()
+    if x_t <= stride_length and x_t >= 0.0:
+        x_t = x_t + v * k_v * dt
+    elif x_t > stride_length:
+        k_v = -1
+        x_t = stride_length
+    elif x_t < 0.0:
+        k_v = 1
+        x_t = 0.0
 
-    #if pos < math.pi/2.0:
-    #    pos = pos + math.pi/3600.0
-    #else:
-    #    pos = 0
+    y_t = a * x_t**4 + b * x_t**3 + c * x_t**2 + d * x_t + e
 
-    #print(pos)
-    #robot.set_joint_positions(pos, 34)
-    sphere.position = np.array([-1.0 + a * np.sin(w*t + np.pi/2.0), -0.25, np.abs(a * np.cos(w*t + np.pi/2.0))])
+
+    sphere.position = np.array([x_t, 0.0, y_t])
     x = robot.get_link_world_positions(link_id)
-    # print("(xd - x) = {}".format(xd - x))
-
-    # perform full IK
     q = robot.calculate_inverse_kinematics(link_id, position=sphere.position)
 
     # set the joint positions
     robot.set_joint_positions(q[q_idx], joint_ids)
 
-    # --
+    # for x in joint_ids:
+    #     #print(x)
+    #     n = robot.get_link_names(x)
+    #     if n != "link16" and n != "link17" and n != "link18" and n != "link19":
+    #         print()
+    #         #robot.set_joint_positions(0.0, x)
+    #     else:
+    #         print("Success")
     #
-    # x = robot.get_link_world_positions(link_id)
-    # dx = robot.get_link_world_linear_velocities(link_id)
-    #
-    # q = robot.get_joint_positions()
-    # J = robot.get_linear_jacobian(link_id, q=q)[:, q_idx]
-    # Jp = robot.get_damped_least_squares_inverse(J, damping)
-    # dq = Jp.dot(kp * (sphere.position - x) - kd * dx)
-    # q = q[q_idx] + dq * sim.dt
-    # robot.set_joint_positions(q, joint_ids=joint_ids)
+    # print("--")
 
-    world.step(sleep_dt=sim.dt)
+    robot.position = np.array([0.9, 0.4, 0.10])
+    world.step(sleep_dt=dt)
